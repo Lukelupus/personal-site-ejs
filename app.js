69858693acbaac906c3e81722ca70b596072b15f
Lucas Boti <luke.boti.lorken@gmail.com>
2022-02-07 10:40:32 -0300

    primeira versão do site curriculo

---
+++ app.js
@@ -0,0 +1,54 @@
+//jshint esversion:6
+
+const express = require("express");
+const ejs = require("ejs");
+const _ = require("lodash");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("home", {
+
+  });
+});
+
+app.get("/skills", function(req, res) {
+  res.render("skills", {
+
+  });
+});
+
+app.get("/profile", function(req, res) {
+  res.render("profile", {
+
+  });
+});
+
+app.get("/portfolio", function(req, res) {
+  res.render("portfolio", {
+
+  });
+});
+
+app.get("/home", function(req, res) {
+  res.render("home", {
+
+  });
+});
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server running on port 3000!");
+});
